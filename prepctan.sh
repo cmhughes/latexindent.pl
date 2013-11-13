b8093634d328b5b909067f1bbb7558ef69030090
cmhughes <christopher.michael.hughes@gmail.com>
2013-11-12 21:06:17 -0800

    prepctan.sh script

---
+++ prepctan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# A little script to help me prepare the bundle for ctan
+
+mkdir latexindent
+mkdir latexindent/documentation
+mkdir latexindent/success
+cp latexindent.pl latexindent
+cp latexindent.exe latexindent
+cp defaultSettings.yaml latexindent
+cp indent.yaml latexindent
+cp readme.txt latexindent
+cp documentation/manual.* latexindent/documentation
+cp success/*.tex latexindent/success
+zip -r latexindent.zip latexindent
+mv latexindent.zip ~/Desktop
+trash-put latexindent
+exit
