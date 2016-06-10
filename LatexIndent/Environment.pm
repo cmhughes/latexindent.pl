25984fb2f52a5bf999ceeb764d89d123aa26a53b
cmhughes <christopher.michael.hughes@gmail.com>
2016-06-10 15:53:21 +0100

    moved find environments subroutine to Environment module

--- LatexIndent/Environment.pm
+++ LatexIndent/Environment.pm
@@ -5,7 +5,9 @@ package LatexIndent::Environment;
 use strict;
 use warnings;
 use Data::Dumper;
+use Exporter qw/import/;
 our @ISA = "LatexIndent::Document"; # class inheritance, Programming Perl, pg 321
+our @EXPORT_OK = qw/find_environments/;
 our %previouslyFoundSettings;
 
 sub indent{
@@ -110,6 +112,94 @@ sub get_indentation_settings_for_this_object{
             ${$self}{$key} = $value;
     }
 
+    return;
+}
+
+sub find_environments{
+    my $self = shift;
+    while( ${$self}{body} =~ m/
+                (
+                    \\begin\{
+                            (.*?)       # environment name captured into $2
+                           \}           # \begin{<something>} statement
+                            (\R*)?      # possible line breaks (into $3)
+                )                       # begin statement captured into $1
+                (
+                    (?:                 # cluster-only (), don't capture 
+                        (?!             # don't include \begin in the body
+                            (?:\\begin) # cluster-only (), don't capture
+                        ).              # any character, but not \\begin
+                    )*?                 # non-greedy
+                            (\R*)?      # possible line breaks (into $5)
+                )                       # environment body captured into $4
+                (
+                    \\end\{\2\}         # \end{<something>} statement
+                    (\h*)?              # possibly followed by horizontal space
+                )                       # captured into $6
+                (\R)?                   # possibly followed by a line break 
+                /sx){
+
+      # create a new Environment object
+      my $env = LatexIndent::Environment->new(begin=>$1,
+                                              name=>$2,
+                                              body=>$4,
+                                              end=>$6,
+                                              linebreaksAtEnd=>{
+                                                begin=> ($3)?1:0,
+                                                body=> ($5)?1:0,
+                                                end=> ($8)?1:0,
+                                              },
+                                            );
+
+      # get settings for this object
+      $env->get_indentation_settings_for_this_object;
+
+      $self->logger(Dumper(\%{$env}),'trace');
+
+      # give unique id
+      $env->create_unique_id;
+
+      # log file output
+      $self->logger("environment found: $2");
+
+      # the replacement text can be just the ID, but the ID might have a line break at the end of it
+      my $replacementText = ${$env}{id};
+
+      # add a line break after \begin{statement} if appropriate
+      if(${$env}{BodyStartsOnOwnLine} and !${$env}{linebreaksAtEnd}{begin}){
+          $self->logger("Adding a linebreak at the end of begin, ${$env}{begin} (see BodyStartsOnOwnLine)");
+          ${$env}{begin} .= "\n";       
+          ${$env}{linebreaksAtEnd}{begin} = 1;
+       }
+
+      # add a line break after body, if appropriate
+      if(${$env}{EndStartsOnOwnLine} and !${$env}{linebreaksAtEnd}{body}){
+          $self->logger("Adding a linebreak at the end of body, ${$env}{body} (see EndStartsOnOwnLine)");
+          ${$env}{body} .= "\n";
+          ${$env}{linebreaksAtEnd}{body} = 1;
+      }
+
+      # line break checks after \end{statement} if appripriate
+      if(${$env}{EndFinishesWithLineBreak} and !${$env}{linebreaksAtEnd}{end}){
+          $self->logger("Adding a linebreak at the end of ${$env}{end} (see EndFinishesWithLineBreak)");
+          ${$env}{end} =~ s/\h*//g;
+          ${$env}{linebreaksAtEnd}{end} = 1;
+          $replacementText .= "\n";
+      }
+
+      # store children in special hash
+      ${$self}{children}{${$env}{id}}=$env;
+
+      # remove the environment block, and replace with unique ID
+      ${$self}{body} =~ s/
+                (\\begin\{(.*?)\}   # the \begin{<something>} statement
+                (\R*)?)             # possible line breaks
+                (((?!(\\begin)).)*?)
+                (\\end\{\2\}(\h*)?)       # the \end{<something>} statement
+                /$replacementText/sx;
+
+      $self->logger("replaced with ID: ${$env}{id}");
+    } 
     return;
 }
 
