1212215472976a3a3f424192d9fa61f5b1101a08
cmhughes <christopher.michael.hughes@gmail.com>
2016-12-06 18:41:27 +0000

    Battery of test cases, see test-cases/keyEqualsValueBraces/pgfkeys-remove-line-breaks-*mod*.tex

--- test-cases/keyEqualsValueBraces/key-equals-values-test-cases.sh
+++ test-cases/keyEqualsValueBraces/key-equals-values-test-cases.sh
@@ -42,9 +42,9 @@ do
    latexindent.pl -s -tt contributors.bib -m -l=mand-args-keywords$i.yaml -o=contributors-mod$i.bib
    # remove line breaks, with trailing comments
    latexindent.pl pgfkeys-remove-line-breaks-first-tc.tex -s -tt -m -l=unprotect-blank-lines.yaml,mand-args-mod$i.yaml -o=pgfkeys-remove-line-breaks-first-tc-mod$i.tex
-   ##latexindent.pl -s -tt pgfkeys-remove-line-breaks.tex -m -l=mand-args-mod$i.yaml -o=pgfkeys-remove-line-breaks-mod$i.tex -g=one.log
-   ##latexindent.pl -s -tt pgfkeys-remove-line-breaks.tex -m -l=mand-args-mod$i.yaml,unprotect-blank-lines.yaml -o=pgfkeys-remove-line-breaks-unprotect-mod$i.tex -g=two.log
-   ##latexindent.pl -s -tt pgfkeys-remove-line-breaks.tex -m -l=mand-args-mod$i.yaml,unprotect-blank-lines.yaml,noCondenseMultipleLines.yaml -o=pgfkeys-remove-line-breaks-unprotect-nocondense-mod$i.tex -g=three.log
+   latexindent.pl -s -tt pgfkeys-remove-line-breaks.tex -m -l=mand-args-mod$i.yaml -o=pgfkeys-remove-line-breaks-mod$i.tex -g=one.log
+   latexindent.pl -s -tt pgfkeys-remove-line-breaks.tex -m -l=mand-args-mod$i.yaml,unprotect-blank-lines.yaml -o=pgfkeys-remove-line-breaks-unprotect-mod$i.tex -g=two.log
+   latexindent.pl -s -tt pgfkeys-remove-line-breaks.tex -m -l=mand-args-mod$i.yaml,unprotect-blank-lines.yaml,noCondenseMultipleLines.yaml -o=pgfkeys-remove-line-breaks-unprotect-nocondense-mod$i.tex -g=three.log
    [[ $silentMode == 0 ]] && set +x 
 done
 ###### NEED TO TEST key STARTS ON OWN LINE!!!
