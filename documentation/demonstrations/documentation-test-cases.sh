6d354a507908238214189a016a059f0abd1d1746
cmhughes <christopher.michael.hughes@gmail.com>
2017-01-14 08:19:09 +0000

    More updates to documentation before tackling alignment bug in command

--- documentation/demonstrations/documentation-test-cases.sh
+++ documentation/demonstrations/documentation-test-cases.sh
@@ -7,9 +7,15 @@ loopmax=0
 # if silentMode is not active, verbose
 [[ $silentMode == 0 ]] && set -x 
 
+# demonstration
 latexindent.pl -s filecontents1.tex -o filecontents1-default.tex
 latexindent.pl -s tikzset.tex -o tikzset-default.tex
 latexindent.pl -s pstricks.tex --outputfile pstricks-default.tex
 latexindent.pl -s pstricks.tex --outputfile pstricks-default.tex -logfile cmh.log
+
+# alignment
+latexindent.pl -s tabular1.tex --outputfile tabular1-default.tex -logfile cmh.log
+latexindent.pl -s -l tabular.yaml tabular1.tex --outputfile tabular1-advanced.tex 
+latexindent.pl -s -l tabular1.yaml tabular1.tex --outputfile tabular1-advanced-3spaces.tex 
 [[ $noisyMode == 1 ]] && makenoise
 git status
