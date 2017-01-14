23a2dafb748bc44ceaeb81c26e7a7938af223dc2
cmhughes <christopher.michael.hughes@gmail.com>
2017-01-14 16:32:13 +0000

    A fair bit of work on documentation, in particular, some wrapfig implementations which looks quite pleasing

--- documentation/demonstrations/documentation-test-cases.sh
+++ documentation/demonstrations/documentation-test-cases.sh
@@ -17,5 +17,15 @@ latexindent.pl -s pstricks.tex --outputfile pstricks-default.tex -logfile cmh.lo
 latexindent.pl -s tabular1.tex --outputfile tabular1-default.tex -logfile cmh.log
 latexindent.pl -s -l tabular.yaml tabular1.tex --outputfile tabular1-advanced.tex 
 latexindent.pl -s -l tabular1.yaml tabular1.tex --outputfile tabular1-advanced-3spaces.tex 
+latexindent.pl -s matrix1.tex -o matrix1-default.tex
+# items
+latexindent.pl -s items1.tex -o items1-default.tex
+# special
+latexindent.pl -s special1.tex -o special1-default.tex
+# headings
+latexindent.pl -s headings1.tex -o headings1-mod1.tex -l=headings1.yaml
+latexindent.pl -s headings1.tex -o headings1-mod2.tex -l=headings2.yaml
+# previously important example
+latexindent.pl -s previously-important-example.tex -o previously-important-example-default.tex
 [[ $noisyMode == 1 ]] && makenoise
 git status
