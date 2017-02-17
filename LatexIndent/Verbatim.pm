409d7377fb44b1613388447b6e22f49e26d34272
cmhughes <christopher.michael.hughes@gmail.com>
2017-02-17 15:55:46 +0000

    Updated latexindent.pl and modules with header information

--- LatexIndent/Verbatim.pm
+++ LatexIndent/Verbatim.pm
@@ -1,7 +1,19 @@
-# Verbatim.pm
-#   creates a class for the Verbatim objects
-#   which are a subclass of the Document object.
 package LatexIndent::Verbatim;
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	See http://www.gnu.org/licenses/.
+#
+#	Chris Hughes, 2017
+#
+#	For all communication, please visit: https://github.com/cmhughes/latexindent.pl
 use strict;
 use warnings;
 use LatexIndent::Tokens qw/%tokens/;
