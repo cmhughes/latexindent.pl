3ab6bd9d05b54eb0535e8325fc62f71c9bca8546
cmhughes <christopher.michael.hughes@gmail.com>
2017-02-21 18:37:20 +0000

    Video demonstrations folder updated

--- documentation/demonstrations/video/video-demo.sh
+++ documentation/demonstrations/video/video-demo.sh
@@ -15,9 +15,20 @@ vim -p environments-nested-fourth.tex environments-nested-fourth-default.tex
 latexindent.pl -s environments-nested-fourth.tex -o environments-nested-fourth-def-ind.tex -l change-default-indent.yaml
 vim -p environments-nested-fourth.tex change-default-indent.yaml environments-nested-fourth-def-ind.tex
 
+# customise indentation
+latexindent.pl -s environments-nested-fourth.tex -o environments-nested-fourth-no-add.tex -l no-add-env.yaml
+vim -p environments-nested-fourth.tex no-add-env.yaml environments-nested-fourth-no-add.tex
+
 # modify line breaks
 latexindent.pl -s environments-nested-fourth.tex -o environments-nested-fourth-mod1.tex -l env-mod1.yaml -m
 vim -p environments-nested-fourth.tex env-mod1.yaml environments-nested-fourth-mod1.tex
+
+# command
+vim -p ../mycommand.tex ../mycommand-default.tex
+vim -p ../mycommand.tex ../mycommand-noAdd1.yaml ../mycommand-noAdd1.tex
+vim -p ../mycommand.tex ../mycommand-noAdd2.yaml ../mycommand-noAdd2.tex
+vim -p ../mycommand.tex ../mycommand-noAdd3.yaml ../mycommand-noAdd3.tex
+vim -p ../mycommand.tex ../mycommand-noAdd4.yaml ../mycommand-noAdd4.tex
 
 # pgfkeys
 latexindent.pl -s pgfkeys-simple.tex -o pgfkeys-simple-default.tex
@@ -28,4 +39,8 @@ vim -p pgfkeys-simple.tex keyVal-no-Add-Glob.yaml pgfkeys-simple-no-add.tex
 # table
 latexindent.pl -s table1.tex -o table1-default.tex
 vim -p table1.tex table1-default.tex
+
+# special
+latexindent.pl -s -m special1.tex -o special1-mod1.tex -l special1-cmh.yaml
+vim -p special1.tex special1-cmh.yaml special1-mod1.tex
 git status
