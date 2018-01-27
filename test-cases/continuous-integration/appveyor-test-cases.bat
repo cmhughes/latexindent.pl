cd4c7ce987b6ee506cb1dfcfce8d1b40a6f75e26
cmhughes <christopher.michael.hughes@gmail.com>
2018-01-27 16:11:13 +0000

    Implementation of AppVeyor ci

---
+++ test-cases/continuous-integration/appveyor-test-cases.bat
@@ -0,0 +1,16 @@
+echo "hello world"
+echo "again again"
+cp ../environments/environments-nested-fourth.tex ../../
+cp ../environments/env-all-on.yaml ../../localSettings.yaml
+cp ../environments/env-all-on.yaml ../../env-all-on.yaml
+cd ../../
+echo "----------1. basic test--------------\n"
+perl latexindent.pl environments-nested-fourth.tex
+echo "----------2. no file extension--------------\n"
+perl latexindent.pl environments-nested-fourth
+echo "----------3. overwrite--------------\n"
+perl latexindent.pl -w environments-nested-fourth.tex
+echo "----------4. localSettings and modify line breaks--------------\n"
+perl latexindent.pl -l -m environments-nested-fourth
+echo "----------5. localSettings renamed and modify line breaks--------------\n"
+perl latexindent.pl -l=env-all-on -m environments-nested-fourth
