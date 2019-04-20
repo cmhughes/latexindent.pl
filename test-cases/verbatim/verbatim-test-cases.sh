1e209280d418ddd98ee47a76a79c0e5a1ba9c5a3
cmhughes <christopher.michael.hughes@gmail.com>
2019-04-20 21:51:06 +0100

    Verbatim: trailing comments and per-name tests

--- test-cases/verbatim/verbatim-test-cases.sh
+++ test-cases/verbatim/verbatim-test-cases.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-loopmax=0
+loopmax=3
+verbatimTest=1
 . ../common.sh
 
 [[ $silentMode == 0 ]] && set -x 
@@ -8,13 +9,20 @@ latexindent.pl -s  verbatim-starred2 -l=no-indent-star.yaml -o=+-mod1
 
 # m switch tests:
 #   verbatim environment
-for (( i=-1 ; i <= 3 ; i++ )) 
+for (( i=$loopmin ; i <= $loopmax ; i++ )) 
 do
     latexindent.pl -s  -m verbatim1 -l=verb-env$i.yaml -o=+-mod$i
     latexindent.pl -s  -m verbatim2 -l=verb-env$i.yaml -o=+-mod$i
     latexindent.pl -s  -m verbatim3 -l=verb-env$i.yaml -o=+-mod$i
 done
+
+# checking a named version of polyswitch
+latexindent.pl -s  -m verbatim5 -l=verb-env-1-named.yaml -o=+-mod-1-named
+latexindent.pl -s  -m verbatim5 -l=verb-env-1-named.yaml,verb-env-1 -o=+-mod-1-named-mk2
+
+latexindent.pl -s  -m verbatim-trailing-comments -l=verb-env2.yaml -o=+-mod2
 #   verbatim command
 #   verbatim special
 #   verbatim noindent block -- no need to test this one...?
 git status
+verbatimTest=0
