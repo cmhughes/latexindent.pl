14cf1e921af21d7dc107c99e54b0e24c514fbd41
sransara <git@tamingentropy.net>
2019-09-16 16:31:39 -0700

    Print usage tip to STDERR only if STDIN is TTY

--- LatexIndent/LogFile.pm
+++ LatexIndent/LogFile.pm
@@ -168,8 +168,10 @@ ENDQUOTE
         $logger->info("Filename: ${$self}{fileName}");
     } else {
         $logger->info("Reading input from STDIN");
-        my $buttonText = ($FindBin::Script eq 'latexindent.exe') ? 'CTRL+Z followed by ENTER':'CTRL+D';
-        print STDERR "Please enter text to be indented: (press $buttonText when finished)\n";
+        if (-t STDIN) {
+            my $buttonText = ($FindBin::Script eq 'latexindent.exe') ? 'CTRL+Z followed by ENTER':'CTRL+D';
+            print STDERR "Please enter text to be indented: (press $buttonText when finished)\n";
+        }
     }
 
     # log the switches from the user
