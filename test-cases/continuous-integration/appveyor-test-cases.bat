025a8734785e9dc595710fb01651cef60b8cad23
jeanlego (Jean-Philippe Legault) <jeanphilippe.legault@unb.ca>
2020-09-15 14:34:40 -0300

    Search localSettings in CWD as well
    This commit adds the ability to also search for localSettings.yaml in CWD if it was not found in the path of the working file.

--- test-cases/continuous-integration/appveyor-test-cases.bat
+++ test-cases/continuous-integration/appveyor-test-cases.bat
@@ -3,6 +3,8 @@ echo "again again"
 cp ../environments/environments-nested-fourth.tex ../../
 cp ../environments/env-all-on.yaml ../../localSettings.yaml
 cp ../environments/env-all-on.yaml ../../env-all-on.yaml
+mkdir ../../localSettingsTest
+cp ../environments/environments-nested-fourth.tex ../../localSettingsTest/
 cd ../../
 echo "----------1. basic test--------------\n"
 perl latexindent.pl environments-nested-fourth.tex
@@ -12,7 +14,9 @@ echo "----------3. overwrite--------------\n"
 perl latexindent.pl -w environments-nested-fourth.tex
 echo "----------4. localSettings and modify line breaks--------------\n"
 perl latexindent.pl -l -m environments-nested-fourth
-echo "----------5. localSettings renamed and modify line breaks--------------\n"
+echo "----------5. localSettings in CWD--------------\n"
+perl latexindent.pl -l -m localSettingsTest/environments-nested-fourth
+echo "----------6. localSettings renamed and modify line breaks--------------\n"
 perl latexindent.pl -l=env-all-on -m environments-nested-fourth
-echo "----------6. STDIN/cat mode--------------\n"
+echo "----------7. STDIN/cat mode--------------\n"
 perl latexindent.pl < environments-nested-fourth.tex
--- test-cases/continuous-integration/test-travis-ci.sh
+++ test-cases/continuous-integration/test-travis-ci.sh
@@ -2,6 +2,8 @@
 cp ../environments/environments-nested-fourth.tex ../../
 cp ../environments/env-all-on.yaml ../../localSettings.yaml
 cp ../environments/env-all-on.yaml ../../env-all-on.yaml
+mkdir ../../localSettingsTest
+cp ../environments/environments-nested-fourth.tex ../../localSettingsTest/
 cd ../../
 echo "----------1. basic test--------------\n"
 perl latexindent.pl environments-nested-fourth.tex
@@ -11,8 +13,10 @@ echo "----------3. overwrite--------------\n"
 perl latexindent.pl -w environments-nested-fourth.tex
 echo "----------4. localSettings and modify line breaks--------------\n"
 perl latexindent.pl -l -m environments-nested-fourth
-echo "----------5. localSettings renamed and modify line breaks--------------\n"
+echo "----------5. localSettings in CWD--------------\n"
+perl latexindent.pl -l -m localSettingsTest/environments-nested-fourth
+echo "----------6. localSettings renamed and modify line breaks--------------\n"
 perl latexindent.pl -l=env-all-on -m environments-nested-fourth
-echo "----------6. STDIN/cat mode--------------\n"
+echo "----------7. STDIN/cat mode--------------\n"
 cat environments-nested-fourth.tex|perl latexindent.pl
 exit
