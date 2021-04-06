0ae8cb4dc10fef9b666938214d6aa8808e994632
cmhughes <christopher.michael.hughes@gmail.com>
2021-04-06 09:24:25 +0100

    unnamed bracket test case from https://github.com/cmhughes/latexindent.pl/issues/265

--- test-cases/unnamed-braces/un-named-grouping-braces.sh
+++ test-cases/unnamed-braces/un-named-grouping-braces.sh
@@ -5,5 +5,8 @@ loopmax=1
 [[ $silentMode == 0 ]] && set -x 
 
 latexindent.pl -s unnamed.tex -o unnamed-default.tex
+latexindent.pl -s issue-265.tex -o +-mod1 -l issue-265.yaml
+latexindent.pl -s issue-265-second.tex -o +-mod1 -l issue-265.yaml
+
 git status
 [[ $noisyMode == 1 ]] && makenoise
