731013f387bd85affe554ff0e7463e81b4f61ad3
cmhughes <christopher.michael.hughes@gmail.com>
2023-10-08 10:40:53 +0100

    test-cases: update to back-up test-cases

--- test-cases/back-up-tests/back-up-tests.sh
+++ test-cases/back-up-tests/back-up-tests.sh
@@ -4,11 +4,13 @@
 # if silentMode is not active, verbose
 [[ $silentMode == 0 ]] && set -x 
 
-latexindent.pl -s -w -l backup1.yaml myfile
-cat indent.log | grep "see maxNumberOfBackUps" > backup-info1.txt
+latexindent.pl -s -w -l backup1.yaml -g one.log myfile
+cp one.log backup-info1.txt
+perl -p0i -e 's/.*?(Backup\sprocedure)/$1/s' backup-info1.txt
+perl -p0i -e 's/INFO:.*//s' backup-info1.txt
 
-latexindent.pl -s -w -l backup2.yaml myfile
-cat indent.log > backup-info2.txt
+latexindent.pl -s -w -l backup2.yaml -g two.log myfile
+cp two.log backup-info2.txt
 perl -p0i -e 's/.*?(Backup\sprocedure)/$1/s' backup-info2.txt
 perl -p0i -e 's/INFO:.*//s' backup-info2.txt
 
