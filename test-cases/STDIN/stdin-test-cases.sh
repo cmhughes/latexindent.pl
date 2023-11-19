a30cfa927b582f3a95cec1b345dd17d69fd3b3e8
cmhughes <christopher.michael.hughes@gmail.com>
2023-11-19 18:44:38 +0000

    bugfix for STDIN, indentPreamble, https://github.com/cmhughes/latexindent.pl/issues/493

--- test-cases/STDIN/stdin-test-cases.sh
+++ test-cases/STDIN/stdin-test-cases.sh
@@ -5,4 +5,10 @@ loopmax=0
 [[ $silentMode == 0 ]] && set -x 
 cat myfile.tex | latexindent.pl -s -o=myfile-default 
 cat myfile.tex | latexindent.pl -s -o=myfile-default -l=dontMeasure.yaml -
+
+# issue 493
+cat issue-493.tex| latexindent.pl -s -m -l latexindent.yaml -o=issue-493-mod1 -
+latexindent.pl -s -l -m -o=+-mod2 issue-493.tex 
+cat issue-493.tex| latexindent.pl -s -m -l issue-493a.yaml -o=issue-493-mod3 -
+
 [[ $gitStatus == 1 ]] && git status
