1f7a55f0e24aef626ccc42dc4018788ef1f94f61
cmhughes <christopher.michael.hughes@gmail.com>
2023-11-29 17:29:33 +0000

    test cases https://github.com/cmhughes/latexindent.pl/issues/495

--- test-cases/text-wrap/text-wrap-remove-PLB.sh
+++ test-cases/text-wrap/text-wrap-remove-PLB.sh
@@ -271,6 +271,8 @@ latexindent.pl -s -m -r -l issue-450.yaml  issue-450f.tex -o=+-mod1
 latexindent.pl -s -m -l issue-471.yaml  issue-471.tex -o=+-mod1
 
 latexindent.pl -s -m -l issue-487.yaml issue-487.tex -o=+-mod1
+
+latexindent.pl -s -m -l issue-495.yaml issue-495.tex -o=+-mod1
 set +x
 [[ $gitStatus == 1 ]] && git status
 [[ $noisyMode == 1 ]] && makenoise
