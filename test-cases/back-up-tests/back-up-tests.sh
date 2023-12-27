4dad44d1f8bf9e995f15bdb7bc48a50ef2da11bc
cmhughes <christopher.michael.hughes@gmail.com>
2023-12-27 10:44:23 +0000

    test cases updated to include https://github.com/cmhughes/latexindent.pl/issues/505

--- test-cases/back-up-tests/back-up-tests.sh
+++ test-cases/back-up-tests/back-up-tests.sh
@@ -18,6 +18,10 @@ latexindent.pl -s -wd -l backup2.yaml -g three.log myfile
 cp three.log backup-info3.txt
 perl -p0i -e 's/.*?(INFO:\s*\-wd)/$1/s' backup-info3.txt
 
+latexindent.pl -w 新建.tex -g issue-505.log -y 'onlyOneBackUp:1'
+cp issue-505.log issue-505.txt
+perl -p0i -e 's/.*?(INFO:\s*Backup)/$1/s' issue-505.txt
+
 set +x
 [[ $gitStatus == 1 ]] && git status
 [[ $noisyMode == 1 ]] && makenoise
