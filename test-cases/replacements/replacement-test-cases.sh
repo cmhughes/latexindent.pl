401dee696d102fdaf355388af368a171b5628ef3
cmhughes <christopher.michael.hughes@gmail.com>
2024-03-16 14:06:56 +0000

    test cases updated for https://github.com/cmhughes/latexindent.pl/issues/523

--- test-cases/replacements/replacement-test-cases.sh
+++ test-cases/replacements/replacement-test-cases.sh
@@ -89,5 +89,8 @@ latexindent.pl -s -l issue-517.yaml -r issue-517.tex -o=+-mod1
 latexindent.pl -s -l issue-517a.yaml -r issue-517.tex -o=+-mod2
 latexindent.pl -s -l issue-517b.yaml -rv issue-517.tex -o=+-mod3
 
+latexindent.pl -s -l issue-523.yaml -r issue-523.tex -o=+-mod1
+latexindent.pl -s -l issue-523a.yaml -m issue-523.tex -o=+-mod2
+
 [[ $gitStatus == 1 ]] && git status
 [[ $noisyMode == 1 ]] && makenoise
