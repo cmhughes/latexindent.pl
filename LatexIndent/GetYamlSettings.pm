e5864781e06b3ee9290166064aed060db6e9418f
fengzyf <154897680+fengzyf@users.noreply.github.com>
2024-06-09 16:45:43 +0800

    bugfix for Incorrect encoding and Update the content on encoding in the document

--- LatexIndent/GetYamlSettings.pm
+++ LatexIndent/GetYamlSettings.pm
@@ -389,36 +389,8 @@ sub yaml_read_settings {
                 # output the contents of indentconfig to the log file
                 $logger->info( Dump \%{ $userSettings->[0] } );
 
-                # change the encoding of the paths according to the field `encoding`
-                if ( $userSettings and ( ref( $userSettings->[0] ) eq 'HASH' ) and $userSettings->[0]->{encoding} ) {
-                    use Encode;
-                    my $encoding       = $userSettings->[0]->{encoding};
-                    my $encodingObject = find_encoding($encoding);
-
-                    # Check if the encoding is valid.
-                    if ( ref($encodingObject) ) {
-                        $logger->info("*Encoding of the paths is $encoding");
-                        foreach ( @{ $userSettings->[0]->{paths} } ) {
-                            my $temp = $encodingObject->encode("$_");
-                            $logger->info("Transform file encoding: $_ -> $temp");
-                            push( @absPaths, $temp );
-                        }
-                    }
-                    else {
-                        $logger->warn("*encoding \"$encoding\" not found");
-                        $logger->warn("Ignore this setting and will take the default encoding.");
-                        @absPaths = @{ $userSettings->[0]->{paths} };
-                        foreach ( @{ $userSettings->[0]->{paths} } ) {
-                            push( @absPaths, $_ );
-                        }
-                    }
-                }
-                else    # No such setting, and will take the default
-                {
-                    # $logger->info("*Encoding of the paths takes the default.");
-                    foreach ( @{ $userSettings->[0]->{paths} } ) {
-                        push( @absPaths, $_ );
-                    }
+                foreach ( @{ $userSettings->[0]->{paths} } ) {
+                    push( @absPaths, $_ );
                 }
             }
 
