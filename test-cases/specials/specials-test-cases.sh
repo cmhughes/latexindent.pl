827166cf2667b986838e5be2dc06d1c188dd08eb
cmhughes <christopher.michael.hughes@gmail.com>
2024-08-15 17:28:23 +0100

    test cases https://github.com/cmhughes/latexindent.pl/issues/554

--- test-cases/specials/specials-test-cases.sh
+++ test-cases/specials/specials-test-cases.sh
@@ -96,5 +96,7 @@ latexindent.pl -s -l issue-480.yaml issue-480.tex -o=+-mod1
 latexindent.pl -s -l issue-511.yaml issue-511.tex -o=+-mod1
 latexindent.pl -s -l issue-511a.yaml issue-511.tex -o=+-mod2
 
+latexindent.pl -s -l issue-554.yaml issue-554.tex -o=+-mod1
+
 [[ $noisyMode == 1 ]] && paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 [[ $gitStatus == 1 ]] && git status
