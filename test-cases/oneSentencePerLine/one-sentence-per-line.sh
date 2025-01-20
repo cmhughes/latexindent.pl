05a918cfcab0fb82c1f7fc891bd59137095f13b0
cmhughes <christopher.michael.hughes@gmail.com>
2025-01-20 17:38:51 +0000

    test-cases https://github.com/cmhughes/latexindent.pl/issues/575

--- test-cases/oneSentencePerLine/one-sentence-per-line.sh
+++ test-cases/oneSentencePerLine/one-sentence-per-line.sh
@@ -168,6 +168,8 @@ latexindent.pl -s issue-527 -m -o=+-mod1 -l=issue-527.yaml
 
 latexindent.pl -s -l issue-557.yaml -m issue-557.tex -o=+-mod1
 
+latexindent.pl -s -l issue-575.yaml -m issue-575.tex -o=+-mod1
+
 [[ $noisyMode == 1 ]] && makenoise
 [[ $gitStatus == 1 ]] && git status
 
