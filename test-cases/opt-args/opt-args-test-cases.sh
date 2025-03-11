c7b2e807019f41c10f26f081a32fe0108a2b4932
cmhughes <christopher.michael.hughes@gmail.com>
2025-03-11 18:38:01 +0000

    opt-arg test cases continue

--- test-cases/opt-args/opt-args-test-cases.sh
+++ test-cases/opt-args/opt-args-test-cases.sh
@@ -34,23 +34,22 @@ do
  latexindent.pl environments-third-opt-args -m -l=opt-args-remove-all,../environments/env-all-on,opt-args-mod$i,addPercentAfterBegin -s -o=+-mod$i -g=other.log
  set +x
 done
-exit
 
 # multi switches set to 2
-latexindent.pl environments-third-opt-args-remove-linebreaks1-trailing-comments -m -l=opt-args-remove-all,../environments/env-all-on,opt-args-mod1,addPercentAfterBegin -s -o=environments-third-opt-args-remove-linebreaks1-trailing-comments-mod1-addPercentAfterBegin
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,opt-args-mod1,addPercentBeforeBegin -m -s -o=environments-third-opt-args-percent-before-begin
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterEnd -m -s -o=environments-third-opt-args-percent-after-end
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterBody -m -s -o=environments-third-opt-args-percent-after-body
-latexindent.pl -s -m -w environments-first-opt-args-mod-supp1 -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterBody -o=environments-first-opt-args-mod-supp1-mod1 
-latexindent.pl -s environments-first-opt-args-more-comments -m -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterBegin  -o=environments-first-opt-args-more-comments-addPercentAfterBegin
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercentAll -m -s -o=environments-third-opt-args-percent-after-all
+latexindent.pl environments-third-opt-args-remove-linebreaks1-trailing-comments -m -l=opt-args-remove-all,../environments/env-all-on,opt-args-mod1,addPercentAfterBegin -s -o=+-mod1-addPercentAfterBegin
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,opt-args-mod1,addPercentBeforeBegin -m -s -o=+-percent-before-begin
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterEnd -m -s -o=+-percent-after-end
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterBody -m -s -o=+-percent-after-body
+latexindent.pl -s -m -w environments-first-opt-args-mod-supp1 -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterBody -o=+-mod1 
+latexindent.pl -s environments-first-opt-args-more-comments -m -l=opt-args-remove-all,../environments/env-all-on,addPercentAfterBegin  -o=+-addPercentAfterBegin
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercentAll -m -s -o=+-percent-after-all
 
 # noAdditionalIndent experiments   
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent -m -s -o=environments-third-opt-args-noAddtionalIndentScalar
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod1 -m -s -o=environments-third-opt-args-noAddtionalIndentHash-mod1
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod2 -m -s -o=environments-third-opt-args-noAddtionalIndentHash-mod2
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod3 -m -s -o=environments-third-opt-args-noAddtionalIndentHash-mod3
-latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod4 -m -s -o=environments-third-opt-args-noAddtionalIndentHash-mod4
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent -m -s -o=+-noAddtionalIndentScalar
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod1 -m -s -o=+-noAddtionalIndentHash-mod1
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod2 -m -s -o=+-noAddtionalIndentHash-mod2
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod3 -m -s -o=+-noAddtionalIndentHash-mod3
+latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-mod4 -m -s -o=+-noAddtionalIndentHash-mod4
 
 # indent rules
 latexindent.pl environments-third-opt-args -l=opt-args-remove-all,../environments/env-all-on,addPercent-noAdditionalIndent-opt-args-indent-rules1 -m -s -o=environments-third-opt-args-indent-rules1
@@ -70,6 +69,7 @@ latexindent.pl environments-third-opt-args-mod1 -l=opt-args-remove-all,../enviro
 # forrest syntax bug, see https://github.com/cmhughes/latexindent.pl/issues/107
 latexindent.pl -s forrest -o=+-mod1 -y="defaultIndent:' '"
 
+exit 
 # issue 445
 latexindent.pl -s -l issue-445  -m issue-445 -o=+-mod1
 latexindent.pl -s -l issue-445a -m issue-445 -o=+-mod2
