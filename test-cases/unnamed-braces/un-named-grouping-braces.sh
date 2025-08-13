3883028e02626ee449d89e26e8cab3e46ba8db79
torik42 <77811180+torik42@users.noreply.github.com>
2025-08-13 16:10:05 +0200

    Add test-cases for #563

--- test-cases/unnamed-braces/un-named-grouping-braces.sh
+++ test-cases/unnamed-braces/un-named-grouping-braces.sh
@@ -8,6 +8,8 @@ latexindent.pl -s unnamed.tex -o unnamed-default.tex
 latexindent.pl -s issue-265.tex -o +-mod1 -l issue-265.yaml
 latexindent.pl -s issue-265-second.tex -o +-mod1 -l issue-265.yaml
 latexindent.pl -s -l issue-568.yaml issue-568.tex -o=+-mod1
+latexindent.pl -s issue-563.tex -o=+-mod1
+latexindent.pl -s issue-563.tex -m -l=issue-563-mod2.yaml -o=+-mod2
 
 [[ $gitStatus == 1 ]] && git status
 [[ $noisyMode == 1 ]] && makenoise
